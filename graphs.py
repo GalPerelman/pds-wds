b12c83e4d33a3410d6a8051db621a8a91b980ab4
GalPerelman <peligur@gmail.com>
2023-06-24 13:24:35 +0300

    class for plotting optimization results

--- graphs.py
+++ graphs.py
@@ -9,31 +9,37 @@ IEEE33_POS = {0: (0, 0), 1: (1, 0), 2: (2, 0), 3: (3, 0), 4: (4, 0), 5: (5, 0), 
               25: (5, 1), 26: (6, 1), 27: (7, 1), 28: (8, 1), 29: (9, 1), 30: (10, 1), 31: (11, 1), 32: (12, 1)
               }
 
+OPF_TEST = {0: (0, 0), 1: (0, -2), 2: (3, -1)}
 
-def pds_graph(pds, edges_values, nodes_values):
-    df = pds.lines.copy()
-    df['w'] = edges_values.values()
-    G = nx.from_pandas_edgelist(df, source='from_bus', target='to_bus', edge_attr='w')
 
-    nx.set_node_attributes(G, IEEE33_POS, 'coord')
-    nx.set_node_attributes(G, nodes_values, 'size')
-    options = {
-        "font_size": 9,
-        "node_size": 200,
-        "node_color": "white",
-        "edgecolors": "black",
-        "linewidths": 1,
-        "width": 1,
-    }
-    fig, ax = plt.subplots(figsize=(12, 5))
-    nx.draw_networkx(G, pos=IEEE33_POS, ax=ax, **options)
+class OptGraphs:
+    def __init__(self, pds):
+        self.pds = pds
 
-    labels_pos = {k: (v[0]+0.2, v[1]+0.15) for k, v in IEEE33_POS.items()}
-    nx.draw_networkx_labels(G, pos=labels_pos, ax=ax, labels=nodes_values, font_size=8, font_color="blue")
+    def pds_graph(self, edges_values, nodes_values, net_coords):
+        df = self.pds.lines.copy()
+        df['w'] = edges_values.values()
+        G = nx.from_pandas_edgelist(df, source='from_bus', target='to_bus', edge_attr='w')
 
-    edge_labels = {k: round(v, 1) for k, v in nx.get_edge_attributes(G, 'w').items()}
-    nx.draw_networkx_edge_labels(G, IEEE33_POS, ax=ax, edge_labels=edge_labels, font_size=8)
-    plt.subplots_adjust(bottom=0.04, top=0.96, left=0.04, right=0.96)
+        nx.set_node_attributes(G, net_coords, 'coord')
+        nx.set_node_attributes(G, nodes_values, 'size')
+        options = {
+            "font_size": 9,
+            "node_size": 200,
+            "node_color": "white",
+            "edgecolors": "black",
+            "linewidths": 1,
+            "width": 1,
+        }
+        fig, ax = plt.subplots(figsize=(12, 5))
+        nx.draw_networkx(G, pos=net_coords, ax=ax, **options)
+
+        labels_pos = {k: (v[0]+0.2, v[1]+0.15) for k, v in net_coords.items()}
+        nx.draw_networkx_labels(G, pos=labels_pos, ax=ax, labels=nodes_values, font_size=8, font_color="blue")
+
+        edge_labels = {k: round(v, 1) for k, v in nx.get_edge_attributes(G, 'w').items()}
+        nx.draw_networkx_edge_labels(G, net_coords, ax=ax, edge_labels=edge_labels, font_size=8)
+        plt.subplots_adjust(bottom=0.04, top=0.96, left=0.04, right=0.96)
 
 
 def time_series(x, y, ax=None):
@@ -42,4 +48,7 @@ def time_series(x, y, ax=None):
 
     ax.plot(x, y)
     ax.grid()
+
+
+
 
