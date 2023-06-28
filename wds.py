9d6ebafe60ba597d3419a0b86f39a664aa80fa6a
GalPerelman <peligur@gmail.com>
2023-06-28 15:08:20 +0300

    read wds data and initiate the wds class

--- wds.py
+++ wds.py
@@ -8,8 +8,21 @@ class WDS:
     def __init__(self, data_folder):
         self.data_folder = data_folder
 
+        self.nodes = pd.read_csv(os.path.join(self.data_folder, 'nodes.csv'), index_col=0)
+        self.pipes = pd.read_csv(os.path.join(self.data_folder, 'pipes.csv'), index_col=0)
         self.pumps = pd.read_csv(os.path.join(self.data_folder, 'pumps.csv'), index_col=0)
 
+        self.n_nodes = len(self.nodes)
+        self.n_pipes = len(self.pipes)
         self.n_pumps = len(self.pumps)
 
+        self.hw = 120
+        self.get_pipes_resistance()
 
+    def get_pipes_resistance(self):
+        """ Calculate pipes resistance coefficients
+            Head loss along pipes is the coefficient multiplied by: Q ^ 1.852
+            Where Q is the pipe flow in cubic meter per hour
+        """
+        const = (1.1311 * 10 ** 9) * ((1 / self.hw) ** 1.852)
+        self.pipes['R'] = const * ((self.pipes['diameter_m'] * 1000) ** (-4.87)) * self.pipes['length_m']
