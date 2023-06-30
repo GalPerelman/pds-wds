d302cab329352b34d8ab02e310b37a7e3077829f
GalPerelman <peligur@gmail.com>
2023-06-30 19:00:54 +0300

    add wds params file and add basic wds functions

--- wds.py
+++ wds.py
@@ -13,19 +13,38 @@ class WDS:
         self.pumps = pd.read_csv(os.path.join(self.data_folder, 'pumps.csv'), index_col=0)
         self.tanks = pd.read_csv(os.path.join(self.data_folder, 'tanks.csv'), index_col=0)
         self.demands = pd.read_csv(os.path.join(self.data_folder, 'demands.csv'), index_col=0).T  # index=nodes
+        self.tariffs = pd.read_csv(os.path.join(self.data_folder, 'tariffs.csv'), index_col=0)
+
+        # read other parameters
+        with open(os.path.join(self.data_folder, 'params.yaml'), 'r') as f:
+            params = yaml.safe_load(f)
+            self.__dict__.update(params)
 
         self.n_nodes = len(self.nodes)
         self.n_pipes = len(self.pipes)
         self.n_pumps = len(self.pumps)
         self.n_tanks = len(self.tanks)
 
-        self.hw = 120
         self.get_pipes_resistance()
+        self.convert_to_cmh()
+
+    def convert_to_cmh(self):
+        self.demands *= 3600
+
+    @staticmethod
+    def hazen_wiliams(c, d, l):
+        """
+
+        :param c: roughness: (dimensionless)
+        :param d: diamater: (m)
+        :param l: length: (m)
+        :return: head loss (m) coefficient
+        """
+        return (1.1311 * 10 ** 9) * ((1 / c) ** 1.852) * ((d * 1000) ** -4.87) * l
 
     def get_pipes_resistance(self):
         """ Calculate pipes resistance coefficients
             Head loss along pipes is the coefficient multiplied by: Q ^ 1.852
             Where Q is the pipe flow in cubic meter per hour
         """
-        const = (1.1311 * 10 ** 9) * ((1 / self.hw) ** 1.852)
-        self.pipes['R'] = const * ((self.pipes['diameter_m'] * 1000) ** (-4.87)) * self.pipes['length_m']
+        self.pipes['R'] = self.hazen_wiliams(self.pipes['c'], self.pipes['diameter_m'], self.pipes['length_m'])
