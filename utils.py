a7795b586baee0d4dae7bddec20e720cdb1d320c
GalPerelman <peligur@gmail.com>
2023-06-30 10:13:42 +0300

    add function for delta t matrix

--- utils.py
+++ utils.py
@@ -21,11 +21,25 @@ def get_connectivity_mat(edges_data: pd.DataFrame, from_col: str = 'from', to_co
     return mat
 
 
-def get_mat_for_node_type(nodes_data: pd.DataFrame, node_type: str):
+def get_mat_for_node_type(nodes_data: pd.DataFrame, node_type: str, inverse=False):
     """
     generate a matrix that can be multiplied by nodes vector to get nodes of certain type
     returns a NxN matrix that is based on an eye matrix where only nodes from the requested type are 1
     """
     idx = np.where(nodes_data['type'] == node_type, 1, 0)
+    if inverse:
+        # to get a matrix of all types but the input one
+        idx = np.logical_not(idx)
+
     mat = idx * np.eye(len(nodes_data))
+    return mat
+
+
+def get_dt_mat(n):
+    """
+    generate a matrix for representing difference between following time steps
+    for example, change in tank volume: dv = v2 - v1
+    if dv is positive (v2 > v1) water flow from the network into the tank and vice versa
+    """
+    mat = np.eye(n, k=0)[:-1] - np.eye(n, k=1)[:-1]
     return mat
