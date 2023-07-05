1b545d7ac44bc1bab47d2ddafe975bc47fd5bc19
GalPerelman <peligur@gmail.com>
2023-07-05 10:01:36 +0300

    units conversion, add property for piecewise linear segments number and add pumps piecewise linearization

--- wds.py
+++ wds.py
@@ -9,9 +9,11 @@ import utils
 
 pipes_max_flow = {100: 40, 150: 100, 200: 125, 250: 250, 300: 400, 330: 525, 480: 1450, 680: 1750}
 
+
 class WDS:
-    def __init__(self, data_folder):
+    def __init__(self, data_folder, n):
         self.data_folder = data_folder
+        self.n = n  # number of piecewise linear segments
 
         self.nodes = pd.read_csv(os.path.join(self.data_folder, 'nodes.csv'), index_col=0)
         self.pipes = pd.read_csv(os.path.join(self.data_folder, 'pipes.csv'), index_col=0)
@@ -32,10 +34,13 @@ class WDS:
 
         self.get_pipes_resistance()
         self.convert_to_cmh()
-        self.pipes_pl = self.linearize_pipes()
+        self.pipes_pl = self.piecewise_linear()
 
     def convert_to_cmh(self):
         self.demands *= 3600
+        self.pipes['min_flow_cmh'] = self.pipes['min_flow_cms'] * 3600
+        self.pipes['max_flow_cmh'] = self.pipes['max_flow_cms'] * 3600
+        self.pumps['b'] /= (3600 ** 2)
 
     @staticmethod
     def hazen_wiliams(c, d, l):
@@ -54,30 +59,25 @@ class WDS:
         """
         self.pipes['R'] = self.hazen_wiliams(self.pipes['c'], self.pipes['diameter_m'], self.pipes['length_m'])
 
-    @staticmethod
-    def pipes_piecewise_linear(n, d, r):
-        # find diameter to set max flow
-        keys = sorted(pipes_max_flow.keys())
-        idx = bisect.bisect_right(keys, d)
-        next_diam = keys[idx]
-
-        max_flow = pipes_max_flow[next_diam]
-
-        x = np.linspace(0, max_flow, n)
-        y = r * (x ** 1.852)
-
-        f = UnivariateSpline(x, y, k=1, s=0)
-        pl = {}
-        for i in range(len(x)-1):
-            a, b = utils.linear_coefficients_from_two_pints((x[i], float(f(x[i]))), (x[i+1], float(f(x[i+1]))))
-            pl[i] = {'start': (x[i], float(f(x[i]))), 'end': (x[i+1], float(f(x[i+1]))), 'a': a, 'b': b}
-
-        return pl
-
-    def linearize_pipes(self):
+    def piecewise_linear(self):
         pipes_pl = {}
         for pipe_id, row in self.pipes.iterrows():
-            pipes_pl[pipe_id] = self.pipes_piecewise_linear(n=4, d=row['diameter_m']*1000, r=row['R'])
+            if row['type'] == 'pipe':
+                x = np.linspace(-row['max_flow_cmh'], row['max_flow_cmh'], self.n)
+                y = row['R'] * x * (np.abs(x)) ** 1.852
+            if row['type'] == 'pump' or row['type'] == 'turbine':
+                a = self.pumps.loc[pipe_id, 'a']
+                b = self.pumps.loc[pipe_id, 'b']
+                x = np.linspace(0,  row['max_flow_cmh'], self.n)
+                y = a - b * (x ** 2)
+
+            f = UnivariateSpline(x, y, k=1, s=0)
+            pl = {}
+            for i in range(len(x) - 1):
+                a, b = utils.linear_coefficients_from_two_pints((x[i], float(f(x[i]))), (x[i + 1], float(f(x[i + 1]))))
+                pl[i] = {'start': (x[i], float(f(x[i]))), 'end': (x[i + 1], float(f(x[i + 1]))), 'a': a, 'b': b}
+
+            pipes_pl[pipe_id] = pl
 
         return pipes_pl
 
