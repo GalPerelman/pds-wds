fix sign of pipes headloss to negative - losing head with flow direction

wds.py
@@ -64,7 +64,7 @@ class WDS:
         for pipe_id, row in self.pipes.iterrows():
             if row['type'] == 'pipe':
                 x = np.linspace(-row['max_flow_cmh'], row['max_flow_cmh'], self.n)
-                y = row['R'] * x * (np.abs(x)) ** 1.852
+                y = -row['R'] * x * (np.abs(x)) ** 0.852
             if row['type'] == 'pump' or row['type'] == 'turbine':
                 a = self.pumps.loc[pipe_id, 'a']
                 b = self.pumps.loc[pipe_id, 'b']