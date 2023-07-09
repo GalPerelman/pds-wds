794737585b71e72da1ac59a1d00ebf00d1110246
GalPerelman <peligur@gmail.com>
2023-07-09 11:27:26 +0300

    add piecewise n segments as input

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ from optimization import Opt
 if __name__ == "__main__":
     PDS_DATA = os.path.join('data', 'pds')
     WDS_DATA = os.path.join('data', 'wds')
-    opt = Opt(pds_data=PDS_DATA, wds_data=WDS_DATA, T=24)
+    opt = Opt(pds_data=PDS_DATA, wds_data=WDS_DATA, t=24, n=8)
     opt.solve()
     opt.plot_results(t=0)
 
-    # a = utils.get_mat_for_type(opt.wds.pipes, 'pump')
-    # dh = a @ opt.x['f'] * opt.wds.pipes['R'].values.reshape(-1, 1)
-    # print(dh)
-    plt.show()
     # code.interact(local=locals())
+    plt.show()
